96e846c537b20eac32917d1e043200833279ae99
yujiaheng0506 <yujiaheng@gmail.com>
2026-02-23 12:46:50 +0800

    完成股票列表拉取代码

--- data_acquire.py
+++ data_acquire.py
@@ -1,4 +1,12 @@
 import os
 import tushare as ts
 import pandas as pd
+from dotenv import load_dotenv
 
+load_dotenv()
+token = os.getenv("Tushare_token")
+print(token)
+pro = ts.pro_api(token=token)
+print("Tushare接口初始化成功")
+df = pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
+df.to_csv("股票列表.csv")
